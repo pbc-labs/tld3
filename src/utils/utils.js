ccbfa0ea9662a931ba6d1875c2d6e8049b3f9155
Preethi Kasireddy <iam.preethi.k@gmail.com>
2015-12-09 12:06:05 -0800

    Update column names of chart to be dynamically determined based on chart scale type

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -10,6 +10,7 @@ const utils = {
 
   /**
   @private
+  @function Creates the main SVG element
   @param {Object} element
     @description The main SVG chart element
   @param {Number} width
@@ -32,7 +33,7 @@ const utils = {
 
   /**
   @private
-  @description Used for setting the default margins for all charts
+  @function Used for setting the default margins for all charts
   @returns {Object} Constructor Class for Margins
   */
 
@@ -42,7 +43,7 @@ const utils = {
 
   /**
   @private
-  @description  Used for setting the default height for all charts to 600px
+  @function  Used for setting the default height for all charts to 600px
   @returns {Object} Constructor Class for Height
   */
 
@@ -52,7 +53,7 @@ const utils = {
 
   /**
   @private
-  @description  Used for setting default width for all charts to 300px
+  @function  Used for setting default width for all charts to 300px
   @returns {Object} Constructor Class for Width
   */
 
@@ -60,11 +61,27 @@ const utils = {
     return new Internal.config.Height(300);
   },
 
+  /**
+  @private
+  @function Used for setting an oridinal scale on chart
+  @param {Number} length
+    @description Width or Height property of chart
+  @returns {Object} d3 ordinal scale set to length param
+  */
+
   setOridinalScale(length) {
     const scale = d3.scale.ordinal() // Add check to figure out scale
                     .rangeRoundBands([0, length], 0.1);
     return scale;
   },
+
+  /**
+  @private
+  @function Used for setting a linear scale on chart
+  @param {Number} length
+    @description Width or Height property of chart
+  @returns {Object} d3 linear scale set to length param
+  */
 
   setLinearScale(length) {
     const scale = d3.scale.linear()
@@ -72,17 +89,57 @@ const utils = {
     return scale;
   },
 
+  /**
+  @private
+  @function Sets the input domain of the ordinal scale to the specified array of discrete values. The first element in values will be mapped to the first element in the output range, the second domain value to the second range value, and so on
+  @param {Object} scale
+    @description y-scale or x-scale of the chart
+  @param {Array} data
+    @description chart data values for the column we are working with
+  @param {Object} input
+    @description column name for the current data value
+  */
+
   mapDataDomainToString(scale, data, input) {
     scale.domain(data.map(d => { return d[input]; }));
   },
+
+  /**
+  @private
+  @function Sets the input domain of the linear scale to the specified array of data values. The first element in values will be mapped to the first element in the output range, the second domain value to the second range value, and so on
+  @param {Object} scale
+    @description y-scale or x-scale of the chart
+  @param {Array} data
+    @description chart data values for the column we are working with
+  @param {Object} input
+    @description column name for the current data value
+  */
 
   mapDataDomainToNumber(scale, data, input) {
     scale.domain([0, d3.max(data, d => { return d[input]; })]);
   },
 
+  /**
+  @private
+  @function Sets Axis label on chart
+  @param {String} label
+    @description The string value of the axis label to be set
+  @returns {Object} Constructor Class for Height
+  */
+
   setAxisLabel(label) {
     return new Internal.config.AxisLabel(label);
   },
+
+  /**
+  @private
+  @function Builds d3 axis
+  @param {String} orientation
+    @description The orientation of the scale's ticks ('bottom' vs. 'top', etc)
+  @returns {Object} scale
+    @description y-scale or x-scale of the chart
+  @returns d3 axis
+  */
 
   createAxis(orientation, scale) {
     const axis = d3.svg.axis()
@@ -90,6 +147,21 @@ const utils = {
                  .orient(orientation);
     return axis;
   },
+
+  /**
+  @private
+  @function Sets style properties for chart axis
+  @param {Object} svg
+    @description SCG element of chart
+  @param {String} part
+    @description The part of the axis we are generating
+  @param {String} fill
+    @description Fill color of axis
+  @param {String} stroke
+    @description Stroke style of axis
+  @returns {String} shapeRerendering
+    @description TODO
+  */
 
   setAxisStyle(svg, part, fill, stroke, shapeRerendering) {
     svg.selectAll('.axis').selectAll(part)
