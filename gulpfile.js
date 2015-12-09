da2124ac90fa80b270cf095da204daeeab985ae7
Banun Atina Idris <banunatina@gmail.com>
2015-12-08 17:33:58 -0800

    (setup) Integrate webpack completely to workflow and change pre-commit script to only lint-strict instead of build.

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,11 @@ var del = require('del');
 var webpack = require("webpack");
 // var WebpackDevServer = require("webpack-dev-server");
 var webpackConfig = require("./webpack.config.js");
+var myTestConfig = require("./webpack.test.config.js");
 
-// default that runs on pre-commit
-gulp.task('default', ['clean', 'lint', 'build', 'mocha', 'doc'], function() {
-  console.log('All done!');
+// should run on precommit if in production
+gulp.task('default', ['clean', 'lint-strict', 'build', 'mocha', 'doc', 'stage'], function() {
+  console.log('Staged all changes');
 });
 
 // run jsDoc on all src files
@@ -21,22 +22,28 @@ gulp.task('doc', ['mocha'], shell.task([
 
 //Lint files using Airbnb config ESLinter
 gulp.task('lint', ['clean'], function () {
-  return gulp.src(['src/*.js', 'src/**/*.js', 'test/*.js', '!node_modules/**', '!bower_components/**'])
+  return gulp.src(['src/*.js', 'src/**/*.js', 'test/tests/*.js', 'test/tests.js', '!node_modules/**', '!bower_components/**'])
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('lint-strict', ['clean'], function () {
+  return gulp.src(['src/*.js', 'src/**/*.js', 'test/tests/*.js', 'test/tests.js', '!node_modules/**', '!bower_components/**'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
 // Run mocha tests on compiled files
-gulp.task('mocha', function () {
- return gulp.src('test/legacy/*.js', {read: false})
+gulp.task('mocha', ["webpack:build-dev", "webpack:build-test"], function () {
+ return gulp.src('test/testBundle.js', {read: false})
         // gulp-mocha needs filepaths so you can't have any plugins before it
         .pipe(mocha({reporter: 'nyan'}));
 });
 
 // Remove built es5 and test files
 gulp.task('clean', function () {
-    return del(['dist/*.js', 'test/legacy/*.js']);
+    return del(['dist/*.js', 'test/testBundle.js']);
 });
 
 // Watch config
@@ -49,19 +56,20 @@ var myDevConfig = Object.create(webpackConfig);
 myDevConfig.devtool = "sourcemap";
 myDevConfig.debug = true;
 
-// create a single instance of the compiler to allow caching
+// create a single instance of the compilers to allow caching
+var testCompiler = webpack(myTestConfig);
 var devCompiler = webpack(myDevConfig);
 
 // Build and watch cycle (another option for development)
 // Advantage: No server required, can run app from filesystem
 // Disadvantage: Requests are not blocked until bundle is available,
 //               can serve an old app on refresh
-gulp.task("build-dev", ["webpack:build-dev"], function() {
-	gulp.watch(["src/**/*"], ["webpack:build-dev"]);
+gulp.task("build-dev", ["lint", "webpack:build-dev", "webpack:build-test"], function() {
+	gulp.watch(["src/**/*"], ["lint", "webpack:build-dev", "webpack:build-test", "mocha"]);
 });
 
 
-gulp.task("webpack:build-dev", ['clean'], function(callback) {
+gulp.task("webpack:build-dev", ['clean', 'lint'], function(callback) {
 	// run webpack
 	devCompiler.run(function(err, stats) {
 		if(err) throw new gutil.PluginError("webpack:build-dev", err);
@@ -70,6 +78,20 @@ gulp.task("webpack:build-dev", ['clean'], function(callback) {
 		}));
 		callback();
 	});
+
+
+});
+
+gulp.task("webpack:build-test", ['clean', 'lint'], function (callback) {
+
+  testCompiler.run(function(err, stats) {
+    if(err) throw new gutil.PluginError("webpack:build-dev", err);
+    gutil.log("[webpack:build-dev]", stats.toString({
+      colors: true
+    }));
+    callback();
+  });
+
 });
 
 // Production build
