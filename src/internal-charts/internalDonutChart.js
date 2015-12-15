bb634941a1ee834b0ff6db8f81fb499334c175f1
Colin Seale <ceseale3@gmail.com>
2015-12-14 17:18:13 -0800

    add donut chart

--- src/internal-charts/internalDonutChart.js
+++ src/internal-charts/internalDonutChart.js
@@ -1,4 +1,99 @@
+/*  global d3  */
+import utils from '../utils/utils';
+
 const DonutChart = {
+
+  updateStyle(context) {
+    context.svg.style({
+      font: 'sans-serif',
+      'font-size': '10px',
+      'text-anchor': 'middle',
+    });
+    return context;
+  },
+
+  updateRadius(context) {
+    context.radius = Math.min(context.getHeight, context.getWidth) / 2;
+    return context;
+  },
+
+  updateColors(context) {
+    context.colorScale = d3.scale.ordinal()
+    .range(context.getColors);
+    return context;
+  },
+
+  updateArc(context) {
+    context.arc = d3.svg.arc()
+               .outerRadius(context.radius - 80)
+               .innerRadius(context.radius - 10);
+    return context;
+  },
+
+  updatePie(context) {
+    context.pie = d3.layout.pie()
+               .sort(null)
+               .value(d => { return d[context.linearColumn]; });
+    return context;
+  },
+
+  updateLinearColunm(context, columnName) {
+    context.linearColumn = columnName || utils.getFirstLinearColumn(context.data);
+    return context;
+  },
+
+  updateOrdinalColunm(context, columnName) {
+    context.ordinalColumn = columnName || utils.getFirstOrdinalColumn(context.data);
+    return context;
+  },
+
+  convertData(context) {
+    context.data.forEach(item => {
+      item[context.linearColumn] = Number(item[context.linearColumn]);
+    });
+    return context;
+  },
+
+  updateTranslation(context) {
+    context.svg.attr('transform', 'translate(' + context.getWidth / 2 + ',' + context.getHeight / 2 + ')');
+    return context;
+  },
+
+  buildChartComponents(context) {
+    const g = context.svg.selectAll('.arc')
+        .data(context.pie(context.data))
+        .enter().append('g')
+        .attr('class', 'arc');
+    g.append('path')
+        .attr('d', context.arc)
+        .style('fill', d => { return context.colorScale(d.data[context.ordinalColumn]); });
+    g.append('text')
+        .attr('transform', d => { return 'translate(' + context.arc.centroid(d) + ')'; })
+        .attr('dx', '.35em')
+        .text(d => { return d.data[context.ordinalColumn]; });
+    return context;
+  },
+
+  updateChartComponents(context) {
+    const g = context.svg.selectAll('.arc')
+          .data(context.pie(context.data));
+    g.selectAll('path')
+          .attr('d', context.arc)
+          .style('fill', d => { return context.colorScale(d.data[context.ordinalColumn]); });
+    g.selectAll('text')
+          .attr('transform', d =>{ return 'translate(' + context.arc.centroid(d) + ')'; })
+          .attr('dy', '.35em')
+          .text(d => { return d.data[context.ordinalColumn]; });
+    return context;
+  },
+
+  updateTitle(context) {
+    context.svg.append('text')
+    .attr('class', 'title')
+    .attr('text-anchor', 'middle')
+    .text('Default Tilte');
+    return this;
+  },
 
 };
 
--- src/subModules/DonutChart.js
+++ src/subModules/DonutChart.js
@@ -1,7 +1,7 @@
 /*  global d3  */
 import { ChartMain } from '../core/ChartMain';
 import Internal from './internal';
-import utils from '../utils/utils';
+import InternalDonut from './internalDonutChart';
 
 export class DonutChart extends ChartMain {
   constructor() {
@@ -10,91 +10,50 @@ export class DonutChart extends ChartMain {
 
   build() {
     Internal.selectElement(this);
-    Internal.createSVGElement(this)
-    .updateOrdinalColunm()
-    .updateLinearColunm()
-    .convertData()
-    .updateRadius()
-    .updateColors()
-    .updateArc()
-    .updatePie()
-    .buildChartComponents();
-
+    Internal.createSVGElement(this);
+    InternalDonut.updateStyle(this);
+    InternalDonut.updateTranslation(this);
+    InternalDonut.updateOrdinalColunm(this);
+    InternalDonut.updateLinearColunm(this);
+    InternalDonut.convertData(this);
+    InternalDonut.updateRadius(this);
+    InternalDonut.updateColors(this);
+    InternalDonut.updateArc(this);
+    InternalDonut.updatePie(this);
+    InternalDonut.buildChartComponents(this);
+    InternalDonut.updateTitle(this);
     return this;
   }
 
-  buildChartComponents() {
-    const g = this.svg.selectAll('.arc')
-        .data(this.pie(this.data))
-        .enter().append('g')
-        .attr('class', 'arc');
-    g.append('path')
-        .attr('d', this.arc)
-        .style('fill', d => { return this.colorScale(d.data.age); });
-    g.append('text')
-        .attr('transform', d => { return 'translate(' + this.arc.centroid(d) + ')'; })
-        .attr('dy', '.35em')
-        .text(d => { return d.data.age; });
+  updateColors() {
+    InternalDonut.updateColors(this);
+    InternalDonut.updateChartComponents(this);
     return this;
   }
 
-  updateChartComponents() {
-    this.svg.selectAll('.arc')
-        .data(this.pie(this.data))
-      .selectAll('path')
-        .attr('d', this.arc)
-        .style('fill', d => { return this.colorScale(d.data[this.ordinalColumn]); })
-      .selectAll('text')
-        .attr('transform', d =>{ return 'translate(' + this.arc.centroid(d) + ')'; })
-        .attr('dy', '.35em')
-        .text(d => { return d.data.age; });
+  updateMargins() {
+    this.updateHeight();
+    this.updateWidth();
     return this;
   }
 
-  updateRadius() {
-    this.radius = Math.min(this.getHeight, this.getWidth);
+  updateWidth() {
+    Internal.updateSVGElement(this);
+    InternalDonut.updateTranslation(this);
+    InternalDonut.updateRadius(this);
+    InternalDonut.updateArc(this);
+    InternalDonut.updatePie(this);
+    InternalDonut.updateChartComponents(this);
     return this;
   }
 
-  updateColors(colors) {
-    this.colorScale = d3.scale.ordinal()
-    .range(colors || this.getColors);
-    return this;
-  }
-
-  updateArc() {
-    this.arc = d3.svg.arc()
-               .outerRadius(this.radius - 80)
-               .innerRadius(this.radius - 10);
-    return this;
-  }
-
-  updatePie() {
-    this.pie = d3.layout.pie()
-               .sort(null)
-               .value(d => { return d[this.linearColumn]; });
-    return this;
-  }
-
-  updateLinearColunm(columnName) {
-    this.linearColumn = columnName || utils.getFirstLinearColumn(this.data);
-    return this;
-  }
-
-  updateOrdinalColunm(columnName) {
-    this.ordinalColumn = columnName || utils.getFirstOrdinalColumn(this.data);
-    return this;
-  }
-
-  convertData() {
-    this.data.forEach(item => {
-      item[this.linearColumn] = Number(item[this.linearColumn]);
-    });
-    return this;
-  }
-
-  updateTranslation() {
-    this.svg.attr('transform', 'translate(' + this.getWidth / 2 + ',' + this.getHeight / 2 + ')');
+  updateHeight() {
+    Internal.updateSVGElement(this);
+    InternalDonut.updateTranslation(this);
+    InternalDonut.updateRadius(this);
+    InternalDonut.updateArc(this);
+    InternalDonut.updatePie(this);
+    InternalDonut.updateChartComponents(this);
     return this;
   }
 
