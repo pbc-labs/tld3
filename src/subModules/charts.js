b4435adedef5f440e65d8859d4c0c0777c45cd2f
Colin Seale <ceseale3@gmail.com>
2015-12-17 07:44:43 -0800

    add new charts to options

--- src/subModules/charts.js
+++ src/subModules/charts.js
@@ -2,9 +2,13 @@ import { BarChart } from '../subModules/BarChart';
 import { LineChart } from '../subModules/LineChart';
 import { DonutChart } from '../subModules/DonutChart';
 import { ScatterChart } from '../subModules/ScatterChart';
+<<<<<<< HEAD
 import { WaffleChart } from '../subModules/WaffleChart';
 import { BarChartLeft } from '../subModules/BarChartLeft';
 
+=======
+import { AreaChart } from '../subModules/AreaChart';
+>>>>>>> add new charts to options
 /**
 @private
 @description This holds all our chart types within main library.
@@ -18,6 +22,7 @@ const charts = {
   ScatterChart,
   WaffleChart,
   BarChartLeft,
+  AreaChart,
 };
 
 export default charts;
