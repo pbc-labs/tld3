e7b934e72c68f342c81934ed37d54d58e9a93e2b
Colin Seale <ceseale3@gmail.com>
2015-12-17 08:07:02 -0800

    remove head from merge conflict

--- src/subModules/charts.js
+++ src/subModules/charts.js
@@ -2,13 +2,10 @@ import { BarChart } from '../subModules/BarChart';
 import { LineChart } from '../subModules/LineChart';
 import { DonutChart } from '../subModules/DonutChart';
 import { ScatterChart } from '../subModules/ScatterChart';
-<<<<<<< HEAD
 import { WaffleChart } from '../subModules/WaffleChart';
 import { BarChartLeft } from '../subModules/BarChartLeft';
-
-=======
 import { AreaChart } from '../subModules/AreaChart';
->>>>>>> add new charts to options
+
 /**
 @private
 @description This holds all our chart types within main library.
