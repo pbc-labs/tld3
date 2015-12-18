d6d5d4833305bd64ffacd725e408f2dc67919f37
Colin Seale <ceseale3@gmail.com>
2015-12-17 16:17:18 -0800

    added docs to donut chart

--- src/subModules/DonutChart.js
+++ src/subModules/DonutChart.js
@@ -8,6 +8,12 @@ export class DonutChart extends ChartMain {
     super();
   }
 
+  /**
+  @private
+  @function build
+  @description Builds up the Donut chart
+  @returns {Object} context Chart object
+  */
   build() {
     Internal.selectElement(this);
     Internal.createSVGElement(this);
@@ -25,18 +31,51 @@ export class DonutChart extends ChartMain {
     return this;
   }
 
+  /**
+  @private
+  @function updateChartComponents
+  @description Calls InternalDonut to update the Donut on chart
+  @returns {Object} this Chart object
+  */
+  updateChartComponents() {
+    /* Calls method on InternalDonut to recreate all the chart components. Used to reflect any changes made to property values (e.g. font-size, axis-label, etc.)
+    */
+    InternalDonut.updateChartComponents(this);
+
+    return this;
+  }
+
+  /**
+  @private
+  @function updateColors
+  @description Calls InternalDonut to update color of Donut chart after initial render
+  @param {Array} colors Array of colors to update the chart to
+  @returns {Object} context Chart object
+  */
   updateColors() {
     InternalDonut.updateColors(this);
     InternalDonut.updateChartComponents(this);
     return this;
   }
 
+  /**
+  @private
+  @function updateMargins
+  @description Updates the chart's margin on the element itself
+  @returns {Object} context Chart object
+  */
   updateMargins() {
     this.updateHeight();
     this.updateWidth();
     return this;
   }
 
+  /**
+  @private
+  @function updateWidth
+  @description Updates the chart's width on the element itself
+  @returns {Object} context Chart object
+  */
   updateWidth() {
     Internal.updateSVGElement(this);
     InternalDonut.updateTranslation(this);
@@ -47,6 +86,12 @@ export class DonutChart extends ChartMain {
     return this;
   }
 
+  /**
+  @private
+  @function updateHeight
+  @description Updates the chart's height on the element itself
+  @returns {Object} context Chart object
+  */
   updateHeight() {
     Internal.updateSVGElement(this);
     InternalDonut.updateTranslation(this);
