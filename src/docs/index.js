f0451469a52db68cef12a69e0e334a605ead0226
Preethi <iam.preethi.k@gmail.com>
2016-01-02 13:15:29 -0800

    add index.js file which will serve as our home page for docco with table of contents

---
+++ src/docs/index.js
@@ -0,0 +1,36 @@
+// # tld3.js - a lightweight library that makes building [d3.js](http://d3js.org`/) graphs much simpler. It abstracts the intricacies of d3.js while harnessing the power of its data-driven DOM manipulation.
+
+// # Table of Contents
+
+// ## Core modules
+// * Main lib files and main chart instance
+// #### [ChartMain.js](./ChartMain.html)
+// #### [core.js](./core.html)
+
+// ## Internal modules
+// * Internal chart files which handle all d3 manipulation
+// #### [internal.js](./internal.html)
+// #### [internalArea.js](./internalArea.html)
+// #### [internalBar.js](./internalBar.html)
+// #### [internalBarLeft.js](./internalBarLeft.html)
+// #### [internalDonutChart.js](./internalDonutChart.html)
+// #### [internalLine.js](./internalLine.html)
+// #### [internalScatter.js](./internalScatter.html)
+// #### [internalWaffle.js](./internalWaffle.html)
+
+// ## Chart instance modules
+// * Individual chart type classes
+// #### [AreaChart.js](./AreaChart.html)
+// #### [BarChart.js](./BarChart.html)
+// #### [BarChartLeft.js](./BarChartLeft.html)
+// #### [charts.js](./charts.html)
+// #### [DonutChart.js](./DonutChart.html)
+// #### [LineChart.js](./LineChart.html)
+// #### [ScatterChart.js](./ScatterChart.html)
+// #### [WaffleChart.js](./WaffleChart.html)
+
+// ## Utils
+// * Utility functions
+// #### [errors.js](./errors.html)
+// #### [themes.js](./themes.html)
+// #### [utils.js](./utils.html)
