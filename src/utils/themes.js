02fccfd3b91f3702c6590ea57e2a0321d2fdd757
Colin Seale <ceseale3@gmail.com>
2016-01-01 22:57:15 -0800

    update 'friendly' color theme with more colors

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -4,7 +4,7 @@ Holds various themes we expose to the user
 
 const themes = {
   bubbly: ['#E71D36', '#26408B', '#FF9F1C', '#767B91', '#0FA3B1'],
-  friendly: ['#253494', '#7FCDBB', '#2C7FB8', '#C7E9B4', '#FFFFCC'],
+  friendly: ['#ffffd9', '#edf8b1', '#c7e9b4', '#7fcdbb', '#41b6c4', '#1d91c0', '#225ea8', '#253494'],
   legendary: ['#5BC0EB', '#FDE74C', '#9BC53D', '#E55934', '#FA7921'],
   fun: ['#2274A5', '#F4530E', '#FFBF00', '#E83F6F', '#32936F'],
   water: ['#1C2541', '#3A506B', '#5BC0BE', '#0B132B', '#6FFFE9'],
